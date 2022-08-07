c7ef9165497247a4ef8847c4ba6dff876d9e8277
Marciano Schildmeijer <marciano@kabisa.nl>
2022-08-07 17:50:34 +0200

    Add the feature.

--- app/routes/polls/index.tsx
+++ app/routes/polls/index.tsx
@@ -1,8 +1,19 @@
-import { ActionFunction, json, LoaderFunction } from "@remix-run/node";
+import {
+	ActionFunction,
+	json,
+	LoaderFunction,
+	redirect,
+} from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 import { getAllPolls, getDocumentPollIds, PollData } from "~/utils/polls";
+import { getAdminUser } from "~/utils/user";
 
 export const loader: LoaderFunction = async ({ params }) => {
+	const isAdmin =
+		(await (await getAdminUser()).map((user) => user.role)[0]) === "admin";
+	if (!isAdmin) {
+		return redirect("/");
+	}
 	const data = await getAllPolls();
 
 	const ids = await getDocumentPollIds();
@@ -16,14 +27,15 @@ export default function AllPolls() {
 	return (
 		<section>
 			<h1>All polls</h1>
+			<Link to="/polls/new">Create new poll</Link>
 			<ul>
 				{polls.map((poll: PollData, idx: number) => (
 					<li key={poll.id}>
 						<p>
 							#{poll.pollNumber} - {poll.question}
 						</p>
-						<Link to={`/poll/${docId[idx]}/edit`}>Edit</Link>{" "}
-						<Link to={`/poll/${docId[idx]}`}>Go to poll</Link>
+						<Link to={`/polls/${docId[idx]}/edit`}>Edit</Link>
+						<Link to={`/polls/${docId[idx]}`}>Go to poll</Link>
 					</li>
 				))}
 			</ul>
