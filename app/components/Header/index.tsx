4cd021641724379de3309f89d047cf372780e896
Marciano Schildmeijer <marciano@kabisa.nl>
2022-10-09 12:41:08 +0200

    Finetune design

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,29 +1,44 @@
 import { useAuth } from "~/providers/AuthProvider";
+import styles from "./styles.css";
 
-const Header = () => {
+export function links() {
+	return [{ rel: "stylesheet", href: styles }];
+}
+
+export const Header = () => {
 	const { user, googleLogin, error } = useAuth();
 
 	return (
-		<header>
+		<header className="header">
 			{user ? (
-				<section>
-					<img
-						referrerPolicy="no-referrer"
-						src={user.photoURL || ""}
-						width={50}
-						height={50}
-					/>
-					<span>{user.displayName}</span>
-					<section>
+				<section className="data">
+					<section className="user">
+						<img
+							referrerPolicy="no-referrer"
+							src={user.photoURL || ""}
+							width={50}
+							height={50}
+						/>
+						<span>{user.displayName}</span>
+					</section>
+					<section className="score-container">
 						<span>
-							Total answered polls: {user.firebase.polls.total}
+							Total answered polls:{" "}
+							<span className="score">
+								{user.firebase.polls.total}
+							</span>
 						</span>
 						<span>
 							Total correct answered polls:{" "}
-							{user.firebase.polls.correct}
+							<span className="score">
+								{user.firebase.polls.correct}
+							</span>
 						</span>
 						<span>
-							Current streak {user.firebase.polls.currentStreak}
+							Current streak{" "}
+							<span className="score">
+								{user.firebase.polls.currentStreak}
+							</span>
 						</span>
 						{/* <span>Max streak {user.firebase.polls.maxStreak}</span> */}
 					</section>
@@ -35,5 +50,3 @@ const Header = () => {
 		</header>
 	);
 };
-
-export default Header;
