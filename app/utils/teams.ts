5f4b9f54c2d1c3fe5a0dfcaf96e1f9dd6316db5c
Marciano Schildmeijer <marciano@kabisa.nl>
2022-12-29 16:37:05 +0100

    Team feature season!

---
+++ app/utils/teams.ts
@@ -0,0 +1,53 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import {
+	getFirestore,
+	doc,
+	getDocs,
+	collection,
+	query,
+	setDoc,
+} from "firebase/firestore";
+import { firebaseConfig } from "~/utils/config.client";
+import { DeepPartial } from "./types";
+
+export type Team = {
+	id: string;
+	users: string[];
+	points: {
+		streak: number;
+		total: number;
+	};
+	name: string;
+};
+
+export type UpdateTeam = Pick<DeepPartial<Team>, "id" | "points">;
+
+export const getTeams = async () => {
+	const app =
+		getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+	const db = getFirestore(app);
+
+	const ref = collection(db, "team");
+	const getQuery = query(ref);
+	const querySnapshot = await getDocs(getQuery);
+
+	return querySnapshot.docs.map((item) => item.data());
+};
+
+export const updateTeamById = async <T extends UpdateTeam>(payload: T) => {
+	const app =
+		getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+	const db = getFirestore(app);
+
+	const snapshot = await setDoc(
+		doc(db, "team", payload.id as string),
+		payload,
+		{
+			merge: true,
+		}
+	);
+
+	return snapshot;
+};
