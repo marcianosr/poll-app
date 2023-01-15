423714acefb60d851e562870d94fa5259252fd80
Marciano Schildmeijer <marciano@kabisa.nl>
2023-01-15 14:47:18 +0100

    Add vercel remix.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,7 +12,7 @@ export const loader: LoaderFunction = async ({ params }) => {
 export default function Index() {
 	const { polls: openPoll, closedPolls } = useLoaderData();
 
-	console.log("home");
+	console.log("home!");
 
 	return (
 		<section style={{ display: "flex", justifyContent: "center" }}>
