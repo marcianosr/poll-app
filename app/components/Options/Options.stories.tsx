e56a7707f5327e100d90ef8e8178e7568402c53a
Marciano Schildmeijer <marciano@kabisa.nl>
2023-02-14 12:52:18 +0100

    Fix banner stories, color text and options.

--- app/components/Options/Options.stories.tsx
+++ app/components/Options/Options.stories.tsx
@@ -15,56 +15,47 @@ const Story: ComponentMeta<typeof OptionsComponent> = {
 	argTypes: {},
 };
 
+const answers = [
+	{
+		id: "eioozak-ojnab",
+		placeholder: "Add option",
+		autoFocus: false,
+		type: "radio",
+		value: "FuncA = never; FuncB = (a: number | string) => unknown;",
+	},
+	{
+		autoFocus: false,
+		id: "a8cbc46b-3f27-47be-a1d7-90fc4b97eaae",
+		placeholder: "Add option",
+		type: "radio",
+		value: "FuncA and FuncB are both: (a: number | string) => unknown;",
+	},
+	{
+		id: "fb6a9753-9499-4662-978b-249a9889d4d6",
+		placeholder: "Add option",
+		autoFocus: false,
+		type: "radio",
+		value: "This is invalid syntax: there should be parenthesis around the function signature on line 1",
+	},
+	{
+		placeholder: "Add option",
+		autoFocus: true,
+		id: "97b3d256-ec8d-4740-892d-1b1d665cd65a",
+		type: "radio",
+		value: "FuncA = ((a: number) => unknown) | (a: string) => unknown);\nFuncB = (a: number | string) => unknown;",
+	},
+];
+
 export default Story;
 
 export const Options: ComponentStory<typeof OptionsComponent> = (props) => {
 	return (
 		<OptionsComponent {...props}>
-			<OptionComponent
-				id="test"
-				text="
-				It is part of the EcmaScript Standard"
-			>
-				<OptionVotes voters={largeAmountOfVoters} />
-			</OptionComponent>
-			<OptionComponent
-				id="test"
-				text="
-				Option 2
-"
-			>
-				<OptionVotes voters={largeAmountOfVoters} />
-			</OptionComponent>
-			<OptionComponent
-				id="test"
-				text="
-				It is an API provided by the browser but has it’s own
-				implementations in other environments"
-			/>
-			<OptionComponent
-				id="test"
-				text="
-				Option 4
-				"
-			/>
-			<OptionComponent
-				id="test"
-				text="
-				It is a way to import any programming language In JavaScript"
-			>
-				<OptionVotes voters={smallAmountOfVoters} />
-			</OptionComponent>
-			<OptionComponent
-				id="test"
-				text="
-				It is part of the general internet
-				"
-			/>
-			<OptionComponent
-				id="test"
-				text="
-				Option 5"
-			/>
+			{answers.map((answer) => (
+				<OptionComponent answer={answer}>
+					<OptionVotes voters={largeAmountOfVoters} />
+				</OptionComponent>
+			))}
 		</OptionsComponent>
 	);
 };
