c40d9ef608fe2383f6ef7e287b0d06d21d1c7b5d
Marciano Schildmeijer <marciano@kabisa.nl>
2023-02-14 13:08:57 +0100

    Fix banner block.

--- .storybook/utils.ts
+++ .storybook/utils.ts
@@ -78,3 +78,34 @@ export const largeAmountOfVoters = [
 		},
 	},
 ];
+
+export const answers = [
+	{
+		id: "eioozak-ojnab",
+		placeholder: "Add option",
+		autoFocus: false,
+		type: "radio",
+		value: "FuncA = never; FuncB = (a: number | string) => unknown;",
+	},
+	{
+		autoFocus: false,
+		id: "a8cbc46b-3f27-47be-a1d7-90fc4b97eaae",
+		placeholder: "Add option",
+		type: "radio",
+		value: "FuncA and FuncB are both: (a: number | string) => unknown;",
+	},
+	{
+		id: "fb6a9753-9499-4662-978b-249a9889d4d6",
+		placeholder: "Add option",
+		autoFocus: false,
+		type: "radio",
+		value: "This is invalid syntax: there should be parenthesis around the function signature on line 1",
+	},
+	{
+		placeholder: "Add option",
+		autoFocus: true,
+		id: "97b3d256-ec8d-4740-892d-1b1d665cd65a",
+		type: "radio",
+		value: "FuncA = ((a: number) => unknown) | (a: string) => unknown);\nFuncB = (a: number | string) => unknown;",
+	},
+];
--- app/components/Options/Options.stories.tsx
+++ app/components/Options/Options.stories.tsx
@@ -1,9 +1,8 @@
 import { Options as OptionsComponent } from ".";
 import { Option as OptionComponent } from "../../ui/Option";
-import { OptionVotes as OptionVotesComponent } from "../OptionVotes";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { OptionVotes } from "../OptionVotes";
-import { largeAmountOfVoters } from "../../../.storybook/utils";
+import { answers, largeAmountOfVoters } from "../../../.storybook/utils";
 
 const Story: ComponentMeta<typeof OptionsComponent> = {
 	component: OptionsComponent,
@@ -11,37 +10,6 @@ const Story: ComponentMeta<typeof OptionsComponent> = {
 	args: {},
 	argTypes: {},
 };
-
-const answers = [
-	{
-		id: "eioozak-ojnab",
-		placeholder: "Add option",
-		autoFocus: false,
-		type: "radio",
-		value: "FuncA = never; FuncB = (a: number | string) => unknown;",
-	},
-	{
-		autoFocus: false,
-		id: "a8cbc46b-3f27-47be-a1d7-90fc4b97eaae",
-		placeholder: "Add option",
-		type: "radio",
-		value: "FuncA and FuncB are both: (a: number | string) => unknown;",
-	},
-	{
-		id: "fb6a9753-9499-4662-978b-249a9889d4d6",
-		placeholder: "Add option",
-		autoFocus: false,
-		type: "radio",
-		value: "This is invalid syntax: there should be parenthesis around the function signature on line 1",
-	},
-	{
-		placeholder: "Add option",
-		autoFocus: true,
-		id: "97b3d256-ec8d-4740-892d-1b1d665cd65a",
-		type: "radio",
-		value: "FuncA = ((a: number) => unknown) | (a: string) => unknown);\nFuncB = (a: number | string) => unknown;",
-	},
-];
 
 export default Story;
 
