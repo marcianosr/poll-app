8d78b0ee4244d6ed99dbfd641a3ee8ce74ff820f
Marciano Schildmeijer <marciano@kabisa.nl>
2023-03-15 22:44:29 +0100

    Hide HTML egg 2

--- app/ui/WarningBanner/index.tsx
+++ app/ui/WarningBanner/index.tsx
@@ -1,3 +1,6 @@
+import { useLoaderData } from "@remix-run/react";
+import { LoaderData } from "~/routes/polls/$id";
+import { EggConditional } from "~/seasonal/Egg/EggContainer";
 import { Banner } from "~/ui/Banner";
 import { Title } from "~/ui/Title";
 import { InputTypes } from "~/utils/polls";
@@ -6,16 +9,31 @@ type WarningBannerProps = {
 	pollType: InputTypes;
 };
 
-export const WarningBanner = ({ pollType }: WarningBannerProps) => (
-	<Banner size="wide" icon="🚨" variant="warning">
-		{pollType === "radio" ? (
-			<Title size="md" variant="primary" tag="span">
-				Be careful! Only 1 answer is correct
-			</Title>
-		) : (
-			<Title size="md" variant="primary" tag="span">
-				Be careful! Multiple answers might be correct
-			</Title>
-		)}
-	</Banner>
-);
+export const WarningBanner = ({ pollType }: WarningBannerProps) => {
+	const { poll } = useLoaderData() as LoaderData;
+
+	return (
+		<Banner
+			size="wide"
+			icon={
+				<EggConditional
+					{...(poll.category === "html" && { category: "html" })}
+					fallbackValue="🚨"
+					id="2"
+					size="xs"
+				/>
+			}
+			variant="warning"
+		>
+			{pollType === "radio" ? (
+				<Title size="md" variant="primary" tag="span">
+					Be careful! Only 1 answer is correct
+				</Title>
+			) : (
+				<Title size="md" variant="primary" tag="span">
+					Be careful! Multiple answers might be correct
+				</Title>
+			)}
+		</Banner>
+	);
+};
