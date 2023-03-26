33d5241c0a544b75e9d8a0239641991462f5cc96
Marciano Schildmeijer <marciano@kabisa.nl>
2023-03-26 17:15:13 +0200

    Show points per message.

--- app/compositions/YourVotes/index.tsx
+++ app/compositions/YourVotes/index.tsx
@@ -21,17 +21,74 @@ export function yourVoteStyles() {
 	];
 }
 
+const POINTS_MESSAGE = {
+	0: {
+		icon: "🫠",
+		message:
+			"This time no points are earned, your score being static is confirmed!",
+	},
+	1: {
+		icon: "🤷🏻‍♂️",
+		message: "It certainly is a start, that single point you got!",
+	},
+	2: {
+		icon: "😐",
+		message:
+			"These points you earned are now a pair, this is how it goes only fair!",
+	},
+	3: {
+		icon: "😤",
+		message:
+			"Your score is added up to 3, I'm sure you can do better you would agree!",
+	},
+	4: {
+		icon: "❤️‍🩹",
+		message:
+			"A double pair of points you can see, it's slightly better than 3!",
+	},
+	5: {
+		icon: "🤒",
+		message:
+			"An average score of 5, this competition is real, so these points are needed to survive!",
+	},
+	6: {
+		icon: "🤐",
+		message:
+			"6 will get you somewhere, but it's not the score you want to share!",
+	},
+	7: {
+		icon: "🍀",
+		message: "Lucky 7 is the score, do better and you'll get more!",
+	},
+	8: {
+		icon: "😊",
+		message: "The score is 8, a bigger total score you will update!",
+	},
+	9: {
+		icon: "🎉",
+		message: "To 9 points I cheer 'wahey!', I don't know what else to say!",
+	},
+	10: {
+		icon: "🤩",
+		message:
+			"Maximum score, a boot you'll put, to score and kick your colleagues butt!",
+	},
+};
+
 export const YourVotes = ({ votes, getCorrectAnswers }: YourVotesProps) => {
+	const points = votes
+		.map((vote: any) => vote.points)
+		.reduce((a: number, b: number) => a + b, 0);
+
 	return (
 		<section className="your-votes-block">
 			<Title size="lg" tag="h2" variant="primary">
 				Your votes
 			</Title>
 			<BannerBlock>
-				<Banner size="wide" icon="🏋️">
+				<Banner size="wide" icon={POINTS_MESSAGE[points].icon}>
 					<Title size="md" variant="primary">
-						Lift each other up: Feel free to discuss your vote in a
-						slack thread!
+						{POINTS_MESSAGE[points].message}
 					</Title>
 				</Banner>
 				<section>
@@ -40,6 +97,7 @@ export const YourVotes = ({ votes, getCorrectAnswers }: YourVotesProps) => {
 							<Option
 								key={vote.id}
 								answer={vote}
+								points={vote.points}
 								variant={
 									getCorrectAnswers(vote?.id || "")
 										? "correct"
