1bfb8cadb6d1301023e62490d830a52d3286acb2
Marciano Schildmeijer <marciano@kabisa.nl>
2023-04-23 12:06:40 +0200

    Storybook and remix run with yarn workspaces.

--- ui/.storybook/main.js
+++ ui/.storybook/main.js
@@ -1,17 +1,17 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
-	stories: ["../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
-	addons: [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/addon-interactions",
-	],
-	framework: {
-		name: "@storybook/react-webpack5",
-		options: {},
-	},
-	docs: {
-		autodocs: "tag",
-	},
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
 };
 export default config;
