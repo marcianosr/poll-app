c53d411b5aba90eddf3ca8785276448d37f65193
Marciano Schildmeijer <marciano@kabisa.nl>
2023-04-27 13:59:32 +0200

    Setup basic auth.

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyA73pnN6GcTHTFhjaEuxK0svl9TChiSDUU",
+	authDomain: "polls-d8b3d.firebaseapp.com",
+	projectId: "polls-d8b3d",
+	storageBucket: "polls-d8b3d.appspot.com",
+	messagingSenderId: "994177718093",
+	appId: "1:994177718093:web:58608f76ee43b40c790904",
+	measurementId: "G-3W6P31RECN",
+};
---
+++ packages/frontend/app/util/session.server.ts
@@ -0,0 +1,106 @@
+import { createCookieSessionStorage, redirect } from "@remix-run/node";
+import { auth } from "@marcianosrs/server-auth";
+
+require("dotenv").config({
+	path: `../../.env`,
+});
+
+const sessionSecret = process.env.SESSION_SECRET;
+
+if (!sessionSecret) {
+	throw new Error("SESSION_SECRET must be set");
+}
+let { getSession, commitSession, destroySession } = createCookieSessionStorage({
+	cookie: {
+		name: "__session",
+		secure: true,
+		secrets: [sessionSecret],
+		sameSite: "lax", // to help with CSRF
+		path: "/",
+		maxAge: 60 * 60 * 24 * 5, // 5 days
+		httpOnly: true,
+	},
+});
+
+export const sessionLogin = async (
+	request: Request,
+	idToken: any,
+	redirectTo: any
+) => {
+	const token = await auth.verifyIdToken(idToken);
+	return auth
+		.createSessionCookie(idToken, {
+			expiresIn: 60 * 60 * 24 * 5 * 1000,
+		})
+		.then(
+			(sessionCookie) => {
+				// Set cookie policy for session cookie.
+				return setCookieAndRedirect(request, sessionCookie, redirectTo);
+			},
+			(error) => {
+				return {
+					error: `sessionLogin error!: ${error.message}`,
+				};
+			}
+		);
+};
+
+export const isSessionValid = async (request: Request) => {
+	const session = await getSession(request.headers.get("cookie"));
+
+	try {
+		// Verify the session cookie. In this case an additional check is added to detect
+		// if the user's Firebase session was revoked, user deleted/disabled, etc.
+		const decodedClaims = await auth.verifySessionCookie(
+			session.get("idToken"),
+			true /** checkRevoked */
+		);
+		return { success: true, decodedClaims };
+	} catch (error) {
+		// Session cookie is unavailable or invalid. Force user to login.
+		return { error: error?.message };
+		// throw redirect(redirectTo, {
+		// 	statusText: error?.message,
+		// });
+	}
+};
+
+const setCookieAndRedirect = async (
+	request: Request,
+	sessionCookie: any,
+	redirectTo = "/"
+) => {
+	const session = await getSession(request.headers.get("cookie"));
+	session.set("idToken", sessionCookie);
+	return redirect(redirectTo, {
+		headers: {
+			"Set-Cookie": await commitSession(session),
+		},
+	});
+};
+
+export const sessionLogout = async (request: Request) => {
+	const session = await getSession(request.headers.get("cookie"));
+
+	// Verify the session cookie. In this case an additional check is added to detect
+	// if the user's Firebase session was revoked, user deleted/disabled, etc.
+	return auth
+		.verifySessionCookie(session.get("idToken"), true /** checkRevoked */)
+		.then((decodedClaims) => {
+			return auth.revokeRefreshTokens(decodedClaims?.sub);
+		})
+		.then(async () => {
+			return redirect("/", {
+				headers: {
+					"Set-Cookie": await destroySession(session),
+				},
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+			// Session cookie is unavailable or invalid. Force user to login.
+			return { error: error?.message };
+		});
+};
+
+export { getSession, commitSession, destroySession };
