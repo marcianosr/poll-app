d480cb4cffbede0971c3a2fffa9280c8db8f5e40
Matthijs Groen <matthijs.groen@gmail.com>
2023-10-05 14:01:33 +0200

    Updates

---
+++ packages/frontend/remix.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  // When running locally in development mode, we use the built-in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.ts",
+  serverBuildPath: "api/index.js",
+  serverModuleFormat: "esm",
+
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+};
