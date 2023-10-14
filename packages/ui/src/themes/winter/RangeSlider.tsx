391b41a1f4d6947f02fb0658677767cfc5d31bf4
Matthijs Groen <matthijs.groen@gmail.com>
2023-10-14 23:45:25 +0200

    Minor cleanup

--- packages/ui/src/themes/winter/RangeSlider.tsx
+++ packages/ui/src/themes/winter/RangeSlider.tsx
@@ -5,34 +5,31 @@ import * as styles from "./RangeSlider.module.css";
 import { useThemeSettings } from "./themeSettings";
 
 export const RangeSlider = forwardRef<HTMLInputElement, RangeSliderProps>(
-	({ children, max, min, step, labels, ...props }, ref) => {
-		const settings = useThemeSettings();
-		const listName = `${props.name}-list`;
+    ({ children, labels, ...props }, ref) => {
+        const settings = useThemeSettings();
+        const listName = `${props.name}-list`;
 
-		const styleClasses = [styles.input, settings.snow && styles.snow]
-			.filter((e) => e !== undefined)
-			.join(" ");
+        const styleClasses = [styles.input, settings.snow && styles.snow]
+            .filter((e) => e !== undefined)
+            .join(" ");
 
-		return (
-			<>
-				<input
-					ref={ref}
-					type="range"
-					min={min}
-					max={max}
-					step={step}
-					className={styleClasses}
-					list={listName}
-					{...props}
-				/>
-				<datalist id={listName} className={styles.default.dataList}>
-					{labels.map((label) => (
-						<option key={label.value} value={label.value}>
-							{label.title}
-						</option>
-					))}
-				</datalist>
-			</>
-		);
-	}
+        return (
+            <>
+                <input
+                    ref={ref}
+                    type="range"
+                    className={styleClasses}
+                    list={listName}
+                    {...props}
+                />
+                <datalist id={listName} className={styles.default.dataList}>
+                    {labels.map((label) => (
+                        <option key={label.value} value={label.value}>
+                            {label.title}
+                        </option>
+                    ))}
+                </datalist>
+            </>
+        );
+    }
 );
