62123cb10498b80dad0d64344228199807eaa42d
Matthijs Groen <matthijs.groen@gmail.com>
2023-10-14 23:43:47 +0200

    Minor cleanup

--- packages/ui/src/themes/html/RangeSlider.tsx
+++ packages/ui/src/themes/html/RangeSlider.tsx
@@ -4,27 +4,19 @@ import { RangeSliderProps } from "../../theming/ThemeType";
 import * as styles from "./RangeSlider.module.css";
 
 export const RangeSlider = forwardRef<HTMLInputElement, RangeSliderProps>(
-	({ children, max, min, step, labels, ...props }, ref) => {
-		const listName = `${props.name}-list`;
-		return (
-			<>
-				<input
-					ref={ref}
-					type="range"
-					min={min}
-					max={max}
-					step={step}
-					list={listName}
-					{...props}
-				/>
-				<datalist id={listName} className={styles.default.dataList}>
-					{labels.map((label) => (
-						<option key={label.value} value={label.value}>
-							{label.title}
-						</option>
-					))}
-				</datalist>
-			</>
-		);
-	}
+    ({ children, labels, ...props }, ref) => {
+        const listName = `${props.name}-list`;
+        return (
+            <>
+                <input ref={ref} type="range" list={listName} {...props} />
+                <datalist id={listName} className={styles.default.dataList}>
+                    {labels.map((label) => (
+                        <option key={label.value} value={label.value}>
+                            {label.title}
+                        </option>
+                    ))}
+                </datalist>
+            </>
+        );
+    }
 );
