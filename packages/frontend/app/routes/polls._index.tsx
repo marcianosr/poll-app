886e3458b5c3f9977749f89ee1e2b4db6aee594f
Matthijs Groen <matthijs.groen@gmail.com>
2023-10-18 20:52:40 +0200

    Show questions in storage

--- packages/frontend/app/routes/polls._index.tsx
+++ packages/frontend/app/routes/polls._index.tsx
@@ -7,47 +7,50 @@ import { getSession } from "~/util/session.server";
 import { questionTypeStore, type Poll } from "@marcianosrs/engine";
 
 type LoaderData = {
-	polls: Poll[];
+    polls: Poll[]; // This is a mismatch with the type in firebase at the moment
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
-	const session = await getSession(request.headers.get("cookie"));
+    const session = await getSession(request.headers.get("cookie"));
 
-	await throwIfNotAuthorized(request);
+    await throwIfNotAuthorized(request);
 
-	const response = await fetch(`${API_ENDPOINT}/polls`, {
-		headers: {
-			Authorization: `Bearer ${session.get("accessToken")}`,
-		},
-	});
+    const response = await fetch(`${API_ENDPOINT}/polls`, {
+        headers: {
+            Authorization: `Bearer ${session.get("accessToken")}`,
+        },
+    });
 
-	const data = await response.json();
+    const data = await response.json();
+    console.log(data);
 
-	return json({ polls: data });
+    return json({ polls: data });
 };
 
 export default function Index() {
-	const { polls } = useLoaderData<LoaderData>();
-	console.log(polls, "POLLS");
+    const { polls } = useLoaderData<LoaderData>();
+    console.log(polls, "POLLS");
 
-	const plugin = questionTypeStore.get("memoryQuestion");
+    return (
+        <main>
+            <h1>Polls</h1>
+            <Link to={"/polls/new"}>Create new poll</Link>
 
-	console.log("plugin", plugin.getContentTitle(polls[0].question.data));
+            <ul>
+                {polls.map((poll) => {
+                    const pluginType = poll.question.type;
+                    const plugin = questionTypeStore.get(pluginType);
+                    const name = plugin.getContentTitle(poll.question.data);
 
-	return (
-		<main>
-			<h1>Polls</h1>
-			<Link to={"/polls/new"}>Create new poll</Link>
-
-			{/* <ul>
-				{polls.map((poll) => (
-					<li key={poll.id}>
-						#{poll.no} - {poll.question}
-						<Link to={`/polls/${poll.id}`}>Go to poll</Link>
-						<Link to={`/polls/${poll.id}/edit`}>Edit</Link>
-					</li>
-				))}
-			</ul> */}
-		</main>
-	);
+                    return (
+                        <li key={poll.id}>
+                            # {name}{" "}
+                            <Link to={`/polls/${poll.id}`}>Go to poll</Link>{" "}
+                            <Link to={`/polls/${poll.id}/edit`}>Edit</Link>
+                        </li>
+                    );
+                })}
+            </ul>
+        </main>
+    );
 }
