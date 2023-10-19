bf013f9489076d1866feb69d6c9510cd50684533
Matthijs Groen <matthijs.groen@gmail.com>
2023-10-19 16:42:20 +0200

    Improve predicted zod type

--- packages/form/src/schema/schemaToZod.ts
+++ packages/form/src/schema/schemaToZod.ts
@@ -1,10 +1,43 @@
 import { z } from "zod";
 import { formFieldPlugins } from "../field-plugins";
-import { FieldType, FormSchema, TypedForm } from "@marcianosrs/form-schema";
+import {
+    BaseFixedFormField,
+    BaseObjectFormField,
+    BaseObjectListFormField,
+    BaseOpenFormField,
+    FieldType,
+    FixedOption,
+    FormField,
+    FormSchema,
+    TypedForm,
+} from "@marcianosrs/form-schema";
 import { FormFieldPlugin } from "../types/field-plugin";
 
+export type TypeMapping = {
+    none: never;
+    string: z.ZodString;
+    number: z.ZodNumber;
+    boolean: z.ZodBoolean;
+    unknown: z.ZodUnknown;
+};
+
+export type ValueTypes = keyof TypeMapping;
+
+export type ZodFieldType<TField extends FormField> =
+    TField extends BaseObjectListFormField<unknown, string, infer Schema>
+        ? z.ZodArray<ZodSchemaType<Schema>>
+        : TField extends BaseObjectFormField<unknown, string, infer Schema>
+        ? ZodSchemaType<Schema>
+        : TField extends BaseFixedFormField<string, string, infer FieldType>
+        ? FieldType[number] extends FixedOption<infer ValueType>
+            ? z.ZodType<ValueType>
+            : never
+        : TField extends BaseOpenFormField<string, string, infer TValueType>
+        ? TypeMapping[TValueType & ValueTypes]
+        : z.ZodTypeAny;
+
 export type ZodSchemaType<Fields extends FormSchema> = z.ZodObject<{
-    [Key in Fields[number] as Key["name"] & string]: z.ZodTypeAny;
+    [Field in Fields[number] as Field["name"] & string]: ZodFieldType<Field>;
 }>;
 
 export const schemaToZod = <T extends TypedForm>(
