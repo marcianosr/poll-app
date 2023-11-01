ea855d6b0f875100de08db96c2deaf83f9f7d119
Matthijs Groen <matthijs.groen@gmail.com>
2023-11-01 20:35:57 +0100

    Fix theme switching in storybook

--- packages/ui/src/themes/winter/Button.tsx
+++ packages/ui/src/themes/winter/Button.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { forwardRef } from "react";
-import * as styles from "./Button.module.css";
+import styles from "./Button.module.css";
 import { useThemeSettings } from "./themeSettings";
 import { ButtonProps } from "../../components/Button";
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-    ({ children, ...props }, ref) => {
-        const settings = useThemeSettings();
+	({ children, ...props }, ref) => {
+		const settings = useThemeSettings();
 
-        const styleClasses = [styles.button, settings.snow && styles.snow]
-            .filter((e) => e !== undefined)
-            .join(" ");
+		const styleClasses = [styles.button, settings.snow && styles.snow]
+			.filter((e) => e !== undefined)
+			.join(" ");
 
-        return (
-            <button {...props} ref={ref} className={styleClasses}>
-                {children}
-            </button>
-        );
-    }
+		return (
+			<button {...props} ref={ref} className={styleClasses}>
+				{children}
+			</button>
+		);
+	}
 );
