3be544a2f8118faebfab3f25442eee7c1d750472
Matthijs Groen <matthijs.groen@gmail.com>
2023-11-07 23:47:49 +0100

    Shift dates to plan around daylight saving time

--- packages/frontend/app/util/getPollIndexForDate.spec.ts
+++ packages/frontend/app/util/getPollIndexForDate.spec.ts
@@ -21,24 +21,24 @@ test("returns -1 if the channel has no frequency", () => {
 
 test("returns the index for the question active today", () => {
 	const mockChannel = channelFactory({
-		startedAt: createTimestamp("2023-10-13:10:00.000Z"),
-		queue: ["poll-fri-13", "poll-mo-16", "poll-tu-17", "poll-wed-18"],
+		startedAt: createTimestamp("2023-11-10:10:00.000Z"),
+		queue: ["poll-fri-10", "poll-mo-13", "poll-tu-14", "poll-wed-15"],
 	});
-	const today = new Date("2023-10-17T08:00:00.000Z"); // 4 days later, but contains a weekend
+	const today = new Date("2023-11-15T08:00:00.000Z"); // 4 days later, but contains a weekend
 
 	const pollIndex = getPollIndexForDate(mockChannel, today);
 	expect(pollIndex).toBe(2);
-	expect(mockChannel.queue[pollIndex]).toEqual("poll-tu-17");
+	expect(mockChannel.queue[pollIndex]).toEqual("poll-tu-14");
 });
 
 test("returns the last known index if date is too far in future", () => {
 	const mockChannel = channelFactory({
-		startedAt: createTimestamp("2023-10-13:10:00.000Z"),
-		queue: ["poll-fri-13", "poll-mo-16", "poll-tu-17", "poll-wed-18"],
+		startedAt: createTimestamp("2023-11-10:10:00.000Z"),
+		queue: ["poll-fri-10", "poll-mo-13", "poll-tu-14", "poll-wed-15"],
 	});
-	const today = new Date("2024-10-17T08:00:00.000Z");
+	const today = new Date("2024-10-15T08:00:00.000Z");
 
 	const pollIndex = getPollIndexForDate(mockChannel, today);
 	expect(pollIndex).toBe(3);
-	expect(mockChannel.queue[pollIndex]).toEqual("poll-wed-18");
+	expect(mockChannel.queue[pollIndex]).toEqual("poll-wed-15");
 });
