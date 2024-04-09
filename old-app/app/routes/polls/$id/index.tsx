c23bb0a46fd403d213e024666f9a9551c91299a3
Marciano Schildmeijer <marciano@kabisa.nl>
2024-04-09 10:07:20 +0200

    Style changes.

--- old-app/app/routes/polls/$id/index.tsx
+++ old-app/app/routes/polls/$id/index.tsx
@@ -267,6 +267,11 @@ export default function PollDetail() {
 							admin page
 						</a>
 					</p>
+					<hr />
+					<i>
+						Apache/5.0 Server at
+						https://poll-app-ivory.vercel.app/polls Port 80{" "}
+					</i>
 				</section>
 			)}
 			<div className="page-container">
